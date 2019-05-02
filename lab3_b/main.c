2ebbf8b49fb741e709fc380eab426d62fe094a31
Sergio <sercar88@gmail.com>
2019-05-02 15:00:54 -0500

    intentando timer

--- lab3_b/main.c
+++ lab3_b/main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<pthread.h>
 
-
+#include "hrtimer_x86.h"
 #include "functions.h"
+
+
 #define NUM_THREADS 2
 
 int cuenta=0;
@@ -11,13 +13,14 @@ pthread_mutex_t lock;
 volatile unsigned long mutex;
 ticket_lock_t mutex_ticket;
 
+
 void*func(void* arg)
 {
 	for (int i = 0; i < 50000; ++i)
 	{
 		cuenta++;
 	}
-
+	return NULL;
 }
 
 void*func_mutex(void* arg)
@@ -28,7 +31,7 @@ void*func_mutex(void* arg)
 		cuenta++;
 		pthread_mutex_unlock(&lock);
 	}
-
+	return NULL;
 }
 
 void*func_tas(void* arg)
@@ -39,7 +42,7 @@ void*func_tas(void* arg)
 		cuenta++;
 		unlock(&mutex);
 	}
-
+	return NULL;
 }
 
 void*func_tatas(void* arg)
@@ -50,6 +53,7 @@ void*func_tatas(void* arg)
 		cuenta++;
 		unlock(&mutex);
 	}
+	return NULL;
 }
 
 void*func_tatas_backoff(void* arg)
@@ -60,6 +64,7 @@ void*func_tatas_backoff(void* arg)
 		cuenta++;
 		unlock(&mutex);
 	}
+	return NULL;
 }
 void*func_ticket(void* arg)
 {
@@ -69,6 +74,7 @@ void*func_ticket(void* arg)
 		cuenta++;
 		ticket_unlock(&mutex_ticket);
 	}
+	return NULL;
 }
 
 
@@ -83,21 +89,25 @@ int main(int argc, char const *argv[])
 	mutex=0;
 	mutex_ticket.next_ticket=0;
 	mutex_ticket.now_serving=0;
+	double start,end,time;
 
+	start= gethrtime_x86();
+	printf("start time %f \n",start);
 
 	for (int i = 0; i < NUM_THREADS; ++i)
 	{
 		pthread_create(&threads[i],NULL,func_ticket,NULL);
 	}
 
-
-
-
 	for (int i = 0; i < NUM_THREADS; ++i)
 	{
 		pthread_join(threads[i], NULL);
 	}
+	end= gethrtime_x86();
+	printf("end time %f\n",end);
 
-	printf("%d\n",cuenta );
+	time=end-start;
+
+	printf("resultado: %d\t tiempo: %f \n",cuenta,time );
 	return 0;
 }
---
+++ lab3_b/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include "hrtimer_x86.h"
+
+int main(int argc, char const *argv[])
+{
+	double start,end,time;
+	start=gethrtime_x86();
+	printf("%f\n",start );
+
+	return 0;
+}
